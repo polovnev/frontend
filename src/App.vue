8bec6ecdebce0d20b1a4debb07c99e09e3678c9d
oleksandr_polovnev <polovnevad@gmail.com>
2022-03-17 18:13:22 +0100

    choose laction bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <ChooseLocation @set-location="setLocation"/>
   Локация {{locationName}}
@@ -27,13 +28,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
--- src/components/ChooseLocation.vue
+++ src/components/ChooseLocation.vue
@@ -1,33 +1,58 @@
 <template>
   <button @click="isShowChooseLocation = true">Локация</button>
-  <div v-if="isShowChooseLocation">
-    <div>
-      Страна:
-      <select @change="loadLocations">
-        <option value="">Select Country</option>
-        <option
-          v-for="country in countries"
-          :key="country.id"
-          :value="country.id"
-        >
-          {{ country.name }}
-        </option>
-      </select>
+
+
+  <section class="vh-100" style="background-color: #2779e2; position: fixed; top: 0; left: 0; width:100%" v-if="isShowChooseLocation">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-xl-9">
+          <h1 class="text-white mb-4">Выберите локацию</h1>
+
+          <div class="card" style="border-radius: 15px">
+            <div class="card-body">
+              <div class="row align-items-center pt-4 pb-3">
+                <div class="col-md-3 ps-5">
+                  <h6 class="mb-0">Страна</h6>
+                </div>
+                <div class="col-md-9 pe-5">
+                  <select @change="loadLocations" class="form-select">
+                    <option selected value="">Выберите страну</option>
+                    <option
+                      v-for="country in countries"
+                      :key="country.id"
+                      :value="country.id"
+                    >
+                      {{ country.name }}
+                    </option>
+                  </select>
+                </div>
+              </div>
+
+              <hr class="mx-n3" />
+
+              <div class="row align-items-center py-3" v-if="isShowLocations">
+                <div class="col-md-3 ps-5">
+                  <h6 class="mb-0">Локация</h6>
+                </div>
+                <div class="col-md-9 pe-5">
+                  <select @change="setLocation" class="form-select">
+                    <option value="">Выберите локацию</option>
+                    <option
+                      v-for="location in locations"
+                      :key="location.id"
+                      :value="location.id"
+                    >
+                      {{ location.name }}
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div v-if="isShowLocations">
-      Локация:
-      <select @change="setLocation">
-        <option value="">Select Location</option>
-        <option
-          v-for="location in locations"
-          :key="location.id"
-          :value="location.id"
-        >
-          {{ location.name }}
-        </option>
-      </select>
-    </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -59,7 +84,7 @@ export default {
     },
     setLocation(event) {
       let locationId = event.target.value;
-      let locationName = this.locations.find(x => x.id == locationId).name;
+      let locationName = this.locations.find((x) => x.id == locationId).name;
       console.log(locationName);
       this.$emit("set-location", locationId, locationName);
       this.isShowChooseLocation = false;
